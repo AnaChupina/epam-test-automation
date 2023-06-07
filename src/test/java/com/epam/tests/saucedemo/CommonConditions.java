5ba4ccef7e14d5be164089811c03d8f3142ee814
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-06-07 15:32:49 +0400

    Clean up the CommonConditions class

--- src/test/java/com/epam/tests/saucedemo/CommonConditions.java
+++ src/test/java/com/epam/tests/saucedemo/CommonConditions.java
@@ -3,7 +3,6 @@ package com.epam.tests.saucedemo;
 import com.epam.driver.DriverSingleton;
 import com.epam.service.ScreenShootOnFailureExtension;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
