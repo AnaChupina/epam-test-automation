eee02989f2eae88110c12f5c3201775a901eae61
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-06-19 13:17:06 +0400

    Remove dependencies of some tests from others(SwaggerPetstore)

---
+++ src/test/java/com/epam/tests/api/swagger/petstore/SwaggerStoreTests.java
@@ -0,0 +1,92 @@
+package com.epam.tests.api.swagger.petstore;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SwaggerStoreTests {
+    private static final Logger LOGGER = LogManager.getLogger(SwaggerStoreTests.class);
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://petstore.swagger.io";
+        RestAssured.basePath = "/v2";
+        LOGGER.info("Inside SwaggerStoreTests beforeEach ");
+        RequestSpecification requestSpec = given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 1,\n" +
+                        "  \"petId\": 1,\n" +
+                        "  \"quantity\": 1,\n" +
+                        "  \"shipDate\": \"2023-06-14T11:30:03.006Z\",\n" +
+                        "  \"status\": \"placed\",\n" +
+                        "  \"complete\": true\n" +
+                        "}");
+        requestSpec.when()
+                .post("/store/order");
+        LOGGER.info("Order with ID=1 was created ");
+    }
+    @Test
+    @DisplayName("api_test_store_1")
+    public void placeOrderForPetTest(){
+        given()
+                .contentType (ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 1,\n" +
+                        "  \"petId\": 1,\n" +
+                        "  \"quantity\": 1,\n" +
+                        "  \"shipDate\": \"2023-06-14T11:30:03.006Z\",\n" +
+                        "  \"status\": \"placed\",\n" +
+                        "  \"complete\": true\n" +
+                        "}")
+                .when()
+                .post("/store/order")
+                .then().log().all()
+                .body("complete", equalTo(true))
+                .body("status", equalTo("placed"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_store_2")
+    public void getPurchaseOrderByIDTest_ID_1(){
+        given()
+                .when()
+                .get("/store/order/1")
+                .then().log().all()
+                .body("complete", equalTo(true))
+                .body("status", equalTo("placed"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_store_3")
+    public void deletePurchaseOrderByIDTest_ID_1(){
+        given()
+                .when()
+                .delete("/store/order/1")
+                .then().log().all()
+                .body("message", equalTo("1"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_store_4")
+    public void returnsPetInventoriesByStatus(){
+        given()
+                .when()
+                .get("/store/inventory")
+                .then().log().all()
+                .assertThat().statusCode(200);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        LOGGER.info("Inside SwaggerStoreTests afterEach ");
+        RestAssured.reset();
+    }
+}
---
+++ src/test/java/com/epam/tests/api/swagger/petstore/SwaggerUserTests.java
@@ -0,0 +1,172 @@
+package com.epam.tests.api.swagger.petstore;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SwaggerUserTests {
+    private static final Logger LOGGER = LogManager.getLogger(SwaggerUserTests.class);
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://petstore.swagger.io";
+        RestAssured.basePath = "/v2";
+        LOGGER.info("Inside SwaggerUserTests beforeEach ");
+        RequestSpecification requestSpec = given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 1,\n" +
+                        "  \"username\": \"user1\",\n" +
+                        "  \"firstName\": \"John\",\n" +
+                        "  \"lastName\": \"Smith\",\n" +
+                        "  \"email\": \"JonhSmith@gmail.com\",\n" +
+                        "  \"password\": \"1234\",\n" +
+                        "  \"phone\": \"12345678\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}");
+        requestSpec.when()
+                .post("/user");
+        LOGGER.info("User with username = user1 was created ");
+    }
+    @Test
+    @DisplayName("api_test_user_1")
+    public void createUsersWithArrayTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("[\n" +
+                        "  {\n" +
+                        "    \"id\": 1,\n" +
+                        "    \"username\": \"string\",\n" +
+                        "    \"firstName\": \"string\",\n" +
+                        "    \"lastName\": \"string\",\n" +
+                        "    \"email\": \"string\",\n" +
+                        "    \"password\": \"string\",\n" +
+                        "    \"phone\": \"string\",\n" +
+                        "    \"userStatus\": 0\n" +
+                        "  }\n" +
+                        "]")
+                .when()
+                .post("/user/createWithArray")
+                .then().log().all()
+                .body("message", equalTo("ok"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_2")
+    public void createUsersWithListTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("[\n" +
+                        "  {\n" +
+                        "    \"id\": 1,\n" +
+                        "    \"username\": \"string\",\n" +
+                        "    \"firstName\": \"string\",\n" +
+                        "    \"lastName\": \"string\",\n" +
+                        "    \"email\": \"string\",\n" +
+                        "    \"password\": \"string\",\n" +
+                        "    \"phone\": \"string\",\n" +
+                        "    \"userStatus\": 0\n" +
+                        "  }\n" +
+                        "]")
+                .when()
+                .post("/user/createWithList")
+                .then().log().all()
+                .body("message", equalTo("ok"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_3")
+    public void updateUserInformationTest_UserId_1(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 1,\n" +
+                        "  \"username\": \"user1\",\n" +
+                        "  \"firstName\": \"John\",\n" +
+                        "  \"lastName\": \"Smith\",\n" +
+                        "  \"email\": \"JonhSmith@gmail.com\",\n" +
+                        "  \"password\": \"1234\",\n" +
+                        "  \"phone\": \"12345678\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}")
+                .when()
+                .put("/user/user1")
+                .then().log().all()
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_4")
+    public void getUserInformationTest_UserId_1(){
+        given()
+                .when()
+                .get("/user/user1")
+                .then().log().all()
+                .body("firstName", equalTo("John"))
+                .body("username", equalTo("user1"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_5")
+    public void loginTest_UserId_1(){
+        given()
+                .param("username", "user1")
+                .param("password","1234")
+                .when()
+                .get("/user/login?username=user1&password=1234")
+                .then().log().all()
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_6")
+    public void logoutTest_UserId_1(){
+        given()
+                .when()
+                .get("/user/logout")
+                .then().log().all()
+                .body("message",equalTo("ok"))
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_7")
+    public void createUserTest_UserId_2(){
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 2,\n" +
+                        "  \"username\": \"user2\",\n" +
+                        "  \"firstName\": \"Ron\",\n" +
+                        "  \"lastName\": \"Wesley\",\n" +
+                        "  \"email\": \"RonWesley@gmail.com\",\n" +
+                        "  \"password\": \"4567\",\n" +
+                        "  \"phone\": \"54947868\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}")
+                .when()
+                .post("/user")
+                .then().log().all()
+                .assertThat().statusCode(200);
+    }
+    @Test
+    @DisplayName("api_test_user_8")
+    public void deleteUserTest_UserId_1(){
+        given()
+                .when()
+                .delete("/user/user1")
+                .then().log().all()
+                .body("message",equalTo("user1"))
+                .assertThat().statusCode(200);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        LOGGER.info("Inside SwaggerUserTests afterEach ");
+        RestAssured.reset();
+    }
+}
