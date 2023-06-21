de6630bcbe2404c3fb98b15e7cb0cd3b64e26765
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-06-21 14:40:20 +0400

    Rewrite ship_date in OrderCreator using LocalDateTime

--- src/main/java/com/epam/utils/api/OrderCreator.java
+++ src/main/java/com/epam/utils/api/OrderCreator.java
@@ -3,12 +3,14 @@ package com.epam.utils.api;
 import com.epam.model.swagger.petstore.Order;
 import com.google.gson.Gson;
 
+import java.time.LocalDateTime;
+
 
 public class OrderCreator {
     private static final Integer ORDER_ID = Integer.valueOf(TestDataReader.getTestData("order.id"));
     private static final Integer PET_ID = Integer.valueOf(TestDataReader.getTestData("pet.id"));
     private static final Integer QUANTITY = Integer.valueOf(TestDataReader.getTestData("quantity"));
-    private static final String SHIP_DATE = TestDataReader.getTestData("ship.date");
+    private static final String SHIP_DATE = LocalDateTime.now().toString();
     private static final String STATUS = TestDataReader.getTestData("status");
     private static final Boolean COMPLETE = Boolean.valueOf(TestDataReader.getTestData("complete"));
 
