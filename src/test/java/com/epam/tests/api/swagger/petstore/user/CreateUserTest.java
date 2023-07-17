a50874ecc3aaaa814151ee459586d97c4f64ea8a
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-07-18 01:30:30 +0400

    CreateUserTest: Remove code from setUp and cleanUp methods which create and delete user

--- src/test/java/com/epam/tests/api/swagger/petstore/user/CreateUserTest.java
+++ src/test/java/com/epam/tests/api/swagger/petstore/user/CreateUserTest.java
@@ -1,10 +1,8 @@
 package com.epam.tests.api.swagger.petstore.user;
 
-import com.epam.api.utils.FileHandler;
 import com.epam.api.utils.ObjectToJsonConvertor;
 import com.epam.api.utils.UserDataGenerator;
 import com.epam.tests.base.BaseAPITest;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,12 +18,9 @@ public class CreateUserTest extends BaseAPITest {
     private static final Logger LOGGER = LogManager.getLogger(UserInteractionTest.class);
     private String user;
     private static Response response;
-    private final String username = FileHandler.getDataFromProperties("petstoretestdata.properties","username");
 
     @BeforeEach
     public void setUp() {
-        user = ObjectToJsonConvertor.convertObjectToJson(UserDataGenerator.createUser());
-        response = userHandler.createUser(user);
         LOGGER.info("Inside SwaggerUserTests beforeEach ");
         LOGGER.info("User with username = user1 was created ");
     }
@@ -49,6 +44,7 @@ public class CreateUserTest extends BaseAPITest {
     @Test
     @DisplayName("Create user with username = user1")
     public void createUserTest(){
+        user = ObjectToJsonConvertor.convertObjectToJson(UserDataGenerator.createUser());
         response = userHandler.createUser(user);
         Assertions.assertEquals(HttpURLConnection.HTTP_OK,response.statusCode());
     }
@@ -56,10 +52,5 @@ public class CreateUserTest extends BaseAPITest {
     @AfterEach
     public void cleanUp() {
         LOGGER.info("Inside SwaggerUserTests afterEach ");
-        response = userHandler.deleteUser(username)
-                .contentType(ContentType.JSON)
-                .body("message",equalTo(username))
-                .extract().response();
-        Assertions.assertEquals(HttpURLConnection.HTTP_OK,response.statusCode());
     }
 }
