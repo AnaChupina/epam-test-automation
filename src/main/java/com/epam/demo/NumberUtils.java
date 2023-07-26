aaa8b47abd514874d497799d8d6a0f0819ae1c85
Anastasia_Chupina <anastasia_chupina@epam.com>
2023-07-26 14:24:57 +0400

    Refactor isEven methods using Predicate and lambda expression

--- src/main/java/com/epam/demo/NumberUtils.java
+++ src/main/java/com/epam/demo/NumberUtils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
+import java.util.function.Predicate;
+
 public class NumberUtils {
 
     public static boolean isEven(int number) {
-        return number % 2 == 0;
+        Predicate <Integer> lambda_exp = (n) -> (n % 2) == 0;
+        return lambda_exp.test(number);
     }
     public static int multiplyByFive (int number) {
         return number*5;
